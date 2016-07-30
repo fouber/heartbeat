d9e1e4b7def1801496744ffa1226d9a83240991e
fouber <fouber.NET@gmail.com>
2016-07-31 03:49:23 +0800

    UPD

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ var map = {};
 try {
     map = require('/tmp/map.json');
 } catch(e){}
+var pad = function(str){
+    return ('0' + str).slice(-2);
+};
+var soFar = function(d){
+    var now = Date.now();
+    var diff = Math.round((now - d.getTime()) / 1000);
+    var second = diff % 60;
+    diff = (diff - second)/60;
+    var minute = diff % 60;
+    var hour = (diff - minute)/60;
+    return [pad(hour), pad(minute), pad(second)].join(':');
+};
 app.get('/', function(req, res){
     var now = Date.now();
     var ret = {
@@ -23,13 +35,17 @@ app.get('/', function(req, res){
     for(var key in map){
         if(map.hasOwnProperty(key)){
             var time = map[key];
-            var d = new Date(time) + '';
+            var d = new Date(time);
+            var info = {
+                startAt: d + '',
+                sofar: soFar(d)
+            };
             if(now - time > EXPIRES_TIME){
                 ret.zombie.count++;
-                ret.zombie.items[key] = d;
+                ret.zombie.items[key] = info;
             } else {
                 ret.alive.count++;
-                ret.alive.items[key] = d;
+                ret.alive.items[key] = info;
             }
         }
     }
