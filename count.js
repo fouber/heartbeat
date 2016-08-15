ad7afb2141eee3a9a5eec96bba6951d28d7722bc
fouber <fouber.NET@gmail.com>
2016-08-16 01:09:08 +0800

    bugfix

--- count.js
+++ count.js
@@ -2,10 +2,6 @@ var fs = require('fs');
 var _ = require('./util');
 var DATA_ROOT = __dirname + '/data/';
 var DATA_TOTAL = DATA_ROOT + 'total.json';
-var current = {
-    www: read('www'),
-    m: read('m')
-};
 
 function read(type, d){
     var f = getIncrsPath(type, d);
@@ -21,16 +17,11 @@ function getIncrsPath(type, d){
 }
 
 exports.incrs = function(type){
-    if(current.hasOwnProperty(type)){
-        current[type]++;
-    } else {
-        current[type] = 1;
-    }
     fs.writeFile(getIncrsPath(type), '1', { flag: 'a' });
 };
 
 exports.current = function(type){
-    return current[type] || 0;
+    return read(type || 'www');
 };
 
 exports.total = function(today){
@@ -46,13 +37,15 @@ exports.total = function(today){
         if(today){
             var date = _.time();
             var index = total.date.indexOf(date);
+            var www = exports.current('www');
+            var m = exports.current('m');
             if(index == -1){
                 total.date.push(date);
-                total.data.www.push(current.www || 0);
-                total.data.m.push(current.m || 0);
+                total.data.www.push(www);
+                total.data.m.push(m);
             } else {
-                total.data.www[index] = current.www || 0;
-                total.data.m[index] = current.m || 0;
+                total.data.www[index] = www;
+                total.data.m[index] = m;
             }
         }
     } catch (e){}
